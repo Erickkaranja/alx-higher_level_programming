399d38f2a352449314ee38b02f32bd245d1fe2da
Erickkaranja <karanja99erick@gmail.com>
2023-01-04 12:23:43 +0300

    tasks

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+for numbers in range(0, 99):
+	#print(number = "{}".format(hex(numbers)))
+	print(f'numbers = {numbers:x}')
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/nin/python3
+
+def uppercase(str):
+	for i in str:
+		if ord(i) >= 97 & ord(i) <= 122:
+			i = chr(ord(i) - 32)
+			print("{}".format(i), end="")
+	print("")
