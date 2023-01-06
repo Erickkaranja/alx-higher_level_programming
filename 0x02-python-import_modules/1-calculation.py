6a7eefa3feb1b867b5e7c17764302b48b21a50ec
Erickkaranja <karanja99erick@gmail.com>
2023-01-06 14:45:18 +0300

    calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     print(f'{a} + {b} = {add(a, b)}')
     print(f'{a} - {b} = {sub(a, b)}')
     print(f'{a} * {b} = {mul(a, b)}')
-    print(f'{a} \ {b} = {div(a, b)}')
+    print(f"{a} '\' {b} = {div(a, b)}")
