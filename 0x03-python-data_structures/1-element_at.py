34d9f5d2461bc6d9f1e1d2aec458d91ab3171328
Erickkaranja <karanja99erick@gmail.com>
2023-01-08 21:31:57 +0300

    element index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    '''returns value at index of a list'''
+    if idx != 0 and idx <= len(my_list):
+        return my_list[idx]
+    else:
+        return None
