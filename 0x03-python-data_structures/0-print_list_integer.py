aca19c08eed40dfa0ddf153efbf7a6f866165fce
Erickkaranja <karanja99erick@gmail.com>
2023-01-08 21:01:30 +0300

    print list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#0-print_list_integer.py
+
+'''prints all integers in a list'''
+def print_list_integer(my_list=[]):
+    for index in range(len(my_list)):
+        print(my_list[index])
