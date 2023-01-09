a329bfe695d30ae9f417eb2474b640c62483513d
Erickkaranja <karanja99erick@gmail.com>
2023-01-09 09:33:55 +0300

    copy an idex of a list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    '''replacing value at a given index'''
+    if idx < 0 and idx > len(my_list) - 1:
+        return my_list
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return copy
