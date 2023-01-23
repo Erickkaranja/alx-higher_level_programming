b772e0ea413e06454cf004b20d82f49946c9aae1
Erickkaranja <karanja99erick@gmail.com>
2023-01-23 19:07:16 +0300

    print message

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 
+def safe_print_list(my_list=[], x=0):
+    '''prints x elements of a list'''
+    try:
+        for i in range(x - 1):
+            print(my_list[i])
+    except indexError:
+        print(my_list)
---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    '''divides 2 integers and prints the result'''
+    try:
+    except:
+    finally:
