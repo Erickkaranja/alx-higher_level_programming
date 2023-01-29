c9d50db83a108fe6f9fddb8c0eca31964e9f68aa
Erickkaranja <karanja99erick@gmail.com>
2023-01-29 11:22:21 +0300

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Erick Karanja
+
+class Square:
+    '''defines a square by size'''
+    def __init__(self, size):
+        '''creates square by size
+        args:
+        size(int):The size of the new square.
+        '''
+        self._size = size
