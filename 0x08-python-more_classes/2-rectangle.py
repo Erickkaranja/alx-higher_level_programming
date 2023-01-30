e72118969a1d7aabf504fa6b2c8eea6e6099f01b
Erickkaranja <karanja99erick@gmail.com>
2023-01-30 20:02:12 +0300

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -17,34 +17,34 @@ class Rectangle:
         '''gets/sets the width value of a rectangle'''
         return (self._width)
 
-    @self.setter
+    @width.setter
     def width(self, value):
         '''sets the width value of a rectangle
         args:
         value (int): The value of width.
         '''
-    if not isinstance(value, int):
-        raise TypeError("width must be an integer")
-    elif value < 0:
-        raise ValueError("width must be >= 0")
-    self._width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
 
     @property
     def height(self):
         '''gets/sets the height value of a rectangle'''
         return (self._height)
 
-    @self.setter
+    @height.setter
     def height(self, value):
         '''sets the width value of a rectangle
         args:
         value (int): The value of width.
         '''
-    if not isinstance(value, int):
-        raise TypeError("height must be an integer")
-    elif value < 0:
-        raise ValueError("height must be >= 0")
-    self._height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
 
     def perimeter(self):
         '''finds the perimeter of a rectangle'''
@@ -55,4 +55,4 @@ class Rectangle:
 
     def area(self):
         '''finds area of a rectangle'''
-    return (self._height * self._width)
+        return (self._height * self._width)
