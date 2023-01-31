f9c0ad412065d6d4ce3c6798dea2188b0ee22c92
Erickkaranja <karanja99erick@gmail.com>
2023-01-31 19:56:26 +0300

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author: Erick Karanja
+
+def add_integer(a, b=98):
+    '''function that adds two integers'''
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
