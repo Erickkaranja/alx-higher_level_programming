115ecf20a8dc220d5a4859c41bf678a3fac9f849
Erickkaranja <karanja99erick@gmail.com>
2023-02-06 21:11:41 +0300

    is instance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 # Author :Erick Karanja
+'''Defining is same class function.'''
+
 
 def is_same_class(obj, a_class):
-    '''checks for an isinstance relation'''
+    '''checks for an isinstance relation.
+
+    Args:
+        obj(any): The object to be checked.
+        a_class(type): family of check of obj.
+
+    Returns:
+        if obj is of instance a_class - True.
+        otherwise False.
+    '''
     if type(obj) == a_class:
         return True
     return False
