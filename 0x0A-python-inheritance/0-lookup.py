9856d79ed312da03a4fb1ebea56399e25ca41dad
Erickkaranja <karanja99erick@gmail.com>
2023-02-06 21:03:28 +0300

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 # Author: Erick Karanja
+'''Defining an abject attribute lookup function.'''
+
 
 def lookup(obj):
-    '''prints all  list of available attributes and methods of an object'''
+    '''prints all  list of available attributes and methods of an object.
+    args:
+    obj (object): object to find attribytes to.
+    '''
     return (dir(obj))
