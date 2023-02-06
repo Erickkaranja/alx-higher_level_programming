226ddd17393d073dea04ba12b7f9995c7a0ef95b
Erickkaranja <karanja99erick@gmail.com>
2023-02-06 21:30:16 +0300

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 # Author: Erick Karanja
+'''Defining class inherit_from.'''
+
 
 def inherits_from(obj, a_class):
-    '''checks if obj is a subclass of a_class'''
+    '''checks if obj is a subclass of a_class.
+    Args:
+        abj(any): The object of check.
+        a_class(type): The class of obj check.
+
+    Returns:
+        if issubclass - True otherwise False.
+    '''
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
