79a980bcd06cad6c4bb9c4e984192f9fa5362227
Erickkaranja <karanja99erick@gmail.com>
2023-02-07 13:58:03 +0300

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Author: Erick Karanja.
+'''defining a file reading function.'''
+
+
+def read_file(filename=""):
+    '''Reads a file and prints it to the stdout.
+
+    Args:
+        filename(file):The file to read from.
+    '''
+    with open(filename, encoding = "utf-8") as f:
+        print(f.read(), end="")
