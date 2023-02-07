ca5f6520b07eed5ce0e3ff3173dc3e357a735b06
Erickkaranja <karanja99erick@gmail.com>
2023-02-07 17:02:49 +0300

    load from json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author: Erick Karanja
+'''implementing func load from json.'''
+import json
+
+
+def load_from_json_file(filename):
+    '''creates an Object from a “JSON file”'''
+    json.load(filename)
