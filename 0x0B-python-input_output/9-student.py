bc2606128d7544cfecb052c5e86130db4b05da64
Erickkaranja <karanja99erick@gmail.com>
2023-02-08 15:50:48 +0300

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,10 +3,10 @@
 '''Defining class students.'''
 
 
-class student:
+class Student:
     '''initiliazing class students.'''
 
-    def __init__(self, first_mane, last_name, age):
+    def __init__(self, first_name, last_name, age):
         '''initializes class student object.
         Args:
             first_name (str): students first name.
@@ -15,7 +15,7 @@ class student:
         '''
 
         self.first_name = first_name
-        self.last_name = last_last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
