da44030c08e7f3e91ffe0987eed80c2c16e5b29d
Erickkaranja <karanja99erick@gmail.com>
2023-03-13 20:59:04 +0300

    correction

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,8 +2,9 @@
 
 def new_in_list(my_list, idx, element):
     '''replacing value at a given index'''
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
-    copy = [x for x in my_list]
-    copy[idx] = element
-    return (copy)
+    new_list = my_list.copy()
+    if idx >= 0 and idx <= len(new_list) - 1:
+        new_list[idx] = element
+        return new_list
+    else:
+        return my_list
