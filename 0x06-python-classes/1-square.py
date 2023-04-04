263dedf2b2b1834c77262954aadcd1f352ec8315
Erickkaranja <karanja99erick@gmail.com>
2023-04-04 08:10:41 +0300

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,9 +3,10 @@
 
 class Square:
     '''defines a square by size'''
+
     def __init__(self, size):
-        '''creates square by size
-        args:
-        size(int):The size of the new square.
+        '''initializes a new square.
+        Args:
+            size (int): The size of the new square.
         '''
         self.__size = size
