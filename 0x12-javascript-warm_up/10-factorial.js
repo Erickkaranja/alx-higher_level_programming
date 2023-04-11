692d2ba96e6d814a5693872521b708c38e5dffee
Erickkaranja <karanja99erick@gmail.com>
2023-04-11 21:09:52 +0300

    correction

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 function factorize (n) {
-  return n === 0 || isNaN ? 1 : n * factorize(n - 1);
+  return n === 0 || isNaN(n) ? 1 : n * factorize(n - 1);
 }
 
 console.log(factorize(Number(process.argv[2])));
