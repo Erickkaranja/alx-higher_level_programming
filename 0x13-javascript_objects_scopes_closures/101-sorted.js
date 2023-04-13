d8bb4e2586488ae0f0599fc28c24f0c649eb1aca
Erickkaranja <karanja99erick@gmail.com>
2023-04-13 22:47:44 +0300

    task 11

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const dict = require('./100-data.js').dict;
-let myDict = {};
-for (let key in dict) {
+const dict = require('./101-data.js').dict;
+const myDict = {};
+for (const key in dict) {
   if (myDict[dict[key]] === undefined) {
     myDict[dict[key]] = [key];
-  }
-  else {
+  } else {
     myDict[dict[key]].push(key);
   }
 }
