5abb39c7605c71738effb50aba402906b9750f5f
Erickkaranja <karanja99erick@gmail.com>
2023-04-21 08:22:24 +0300

    style checker

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3],
-                        port=3306)
+                         port=3306)
     c = db.cursor()
     c.execute("SELECT * FROM `states` ORDER BY id ASC")
     [print(state) for state in c.fetchall()]
