d833f38095524c1bb13c77e7e633740ada8dadbf
Erickkaranja <karanja99erick@gmail.com>
2023-04-27 15:10:26 +0300

    adding documentation

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def find_peak(list_of_integers):
+    '''finds the peak integer in a list.'''
     if len(list_of_integers) != 0:
         sorted_numbers = sorted(list_of_integers, reverse=True)
         return sorted_numbers[0]
