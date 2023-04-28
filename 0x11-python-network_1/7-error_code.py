adb47292185caf6302eff2c9fc0cd510a29bea40
Erickkaranja <karanja99erick@gmail.com>
2023-04-29 00:09:05 +0300

    task 7 correction

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ import requests
 
 if __name__ = "__main__":
     url = sys.agv[1]
-    r = requests(url)
+    r = requests.get(url)
     if r.status_code >= 400:
         print("Error code: {}".format(r.sratus_code))
     else:
