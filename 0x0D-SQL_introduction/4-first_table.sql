cd4618e3878d151d8120993dd550bb15d6ee51e6
Erickkaranja <karanja99erick@gmail.com>
2023-07-18 19:36:15 +0300

    correction

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,4 @@
 -- creates a table called first_table in the current database.
-USE DATABASE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS `first_table` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(256)
