4396bde15c03690057e44b9dadf58d2323790e07
Erickkaranja <karanja99erick@gmail.com>
2023-07-26 06:35:35 +0300

    advanced task

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,6 @@ const url = process.argv[2];
 const request = require('request');
 request
   .get(url)
-  .on('response', function(response) {
-    console.log(`code: ${response.statusCode}`)
+  .on('response', function (response) {
+    console.log(`code: ${response.statusCode}`);
   });
